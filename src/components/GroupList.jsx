9c6b44228c7a7a459aaaef3816b37dc7a24f8f79
Bishal-laha <bishallaha12345@gmail.com>
2024-08-02 04:12:52 +0530

    Update GroupList.jsx

--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -7,7 +7,7 @@ const GroupList = ({ width = "100%", groupList, setIsOpen }) => {
     return (
         <div className='relative shadow-inner'>
             <Stack width={width} direction={"column"} height={"90%"} >
-                {groupList?.map((item, id) => <GroupItem key={id} item={item} />)}
+                {groupList.length === 0 ? (<Typography variant="body1" color={"#515159"} textAlign={"center"}>No groups are created till now</Typography>) : (groupList?.map((item, id) => <GroupItem key={id} item={item} />))}
             </Stack>
             <div className="bg-[#16008B] w-10 h-10 text-white rounded-full text-center duration-300 hover:duration-300 hover:scale-110 items-center sticky bottom-[12%] left-[72%]">
                 <button onClick={() => setIsOpen(true)} ><AddIcon fontSize="large" /></button>
